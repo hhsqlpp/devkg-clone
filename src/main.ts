2d7e485abea1a76dccef2d4f6b44f6824eec2ca5
Bekzat Baktybekov <bekzatbaktybekov4@gmail.com>
2022-06-23 12:45:59 +0600

    set port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
 	app.setGlobalPrefix('api');
-	await app.listen(process.env.PORT || 8080);
+	await app.listen(parseInt(process.env.PORT) || 8080);
 }
 bootstrap();
