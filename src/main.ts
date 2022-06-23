1fa6937b39e81be99e939faed1e9ee42cf0a1366
Bekzat Baktybekov <bekzatbaktybekov4@gmail.com>
2022-06-23 13:35:01 +0600

    deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
-	const PORT = parseInt(process.env.PORT) || 8080;
+	app.setGlobalPrefix('api');
+	const PORT = process.env.PORT || 3000;
 	await app.listen(PORT);
 }
 bootstrap();
